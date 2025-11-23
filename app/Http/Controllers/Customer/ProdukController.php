09f5df0ee387cab8a141d572366996727fde8152
SalmaArifahZahra <136414163+SalmaArifahZahra@users.noreply.github.com>
2025-11-23 10:34:28 +0700

    test controllerdetail

--- app/Http/Controllers/Customer/ProdukController.php
+++ app/Http/Controllers/Customer/ProdukController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Produk;
 class ProdukController extends Controller
 {
-    //
+    public function detail($id)
+    {
+        $produk = Produk::with('detail')->findOrFail($id);
+
+        // Produk lainnya untuk rekomendasi
+        $produkLainnya = Produk::with('detail')
+            ->where('id_produk', '!=', $id)
+            ->limit(10)
+            ->get();
+
+        return view('customer.produk.detail');
+    }
 }
